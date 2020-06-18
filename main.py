2aa532af347c31729ac00943c64b1f2ec0e5d48a
Bailey Dalton <baileydalton007@gmail.com>
2020-06-18 16:35:38 -0400

    Adds multiple init arrays

--- main.py
+++ main.py
@@ -13,8 +13,14 @@ cv2.imwrite('assets/screenshot.png', frame)
 errorMargin = 2000
 
 #gives a array of background to compare to
-initArr = screenshot.beforeDart()
+initArr1 = screenshot.beforeDart()
 time.sleep(0.5)
+initArr2 = screenshot.beforeDart()
+time.sleep(0.5)
+initArr3 = screenshot.beforeDart()
+
+initArr = list(set().union(initArr1, initArr2, initArr3))
+
 
 while True:
     (grabbed, frame) = camera.read()
